63fefdc8e20b294a09b480665842edf3b2c4d804
titi0177 <criss0177@gmail.com>
2026-04-14 21:35:03 -0300

    Convert to static SPA - remove TanStack Start SSR

--- api/index.js
+++
@@ -1,8 +0,0 @@
-import { createRequestHandler } from '@tanstack/react-start/server'
-import { getRouterManifest } from './dist/server/index.js'
-
-const handler = createRequestHandler({
-  getRouterManifest,
-})
-
-export default handler
