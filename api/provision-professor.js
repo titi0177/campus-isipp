537f7ab1a92ec57bcca5d7c1794ccc0c3c0c14fb
titi0177 <criss0177@gmail.com>
2026-04-17 09:51:29 -0300

    Fix ES module import syntax in provision professor endpoint

--- api/provision-professor.js
+++ api/provision-professor.js
@@ -1,4 +1,4 @@
-const { createClient } = require('@supabase/supabase-js')
+import { createClient } from '@supabase/supabase-js'
 
 function getServiceSupabase() {
   const url = process.env.SUPABASE_URL
@@ -63,7 +63,7 @@ function passwordFromDni(dni) {
   return digits
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
